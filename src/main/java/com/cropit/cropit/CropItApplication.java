71374e0d6a3e58f11a32d6b845c8b0f66b55f7ae
assingh2 <ashutosh.s.singh@here.com>
2022-10-01 20:38:22 +0530

    Initial commit for Crop IT

---
+++ src/main/java/com/cropit/cropit/CropItApplication.java
@@ -0,0 +1,13 @@
+package com.cropit.cropit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cropit/cropit/CropItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropit.cropit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
